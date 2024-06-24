d0404e280ff755618b2edee00db08a1b849122e3
Pushpanatha <ppreddy2711@gmail.com>
2024-06-24 19:16:41 +0530

    updated three.java and four.java

---
+++ two.java
@@ -0,0 +1,16 @@
+ppred@Pushpanath MINGW64 /d/june-2017/hellogit (master)
+$ git log
+commit 154d294e34811babdf791ed80e5f3c318c438966 (HEAD -> master)
+Author: Pushpanatha <ppreddy2711@gmail.com>
+Date:   Mon Jun 24 19:08:25 2024 +0530
+
+    added four.java and modified one.java
+
+commit a808fbf0ab1cea5da5630021fda0c8697317b46d (origin/master)
+Author: Pushpanatha <ppreddy2711@gmail.com>
+Date:   Mon Jun 24 18:46:39 2024 +0530
+
+    JIRA-1234: Implemented finance logic
+
+ppred@Pushpanath MINGW64 /d/june-2017/hellogit (master)
+$
